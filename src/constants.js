da6b2da86eeb346a616fddad5784093309db2e67
Thomas Cherret <thomas.cherret@papernest.com>
2021-11-26 09:30:48 +0100

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x277492c8054af2693Ade19013906100687963DD0';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
